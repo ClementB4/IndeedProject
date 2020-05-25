6181db753d3b42f7195bf0a37140909619d7e809
clementbrunet42@gmail.com <clementbrunet42@gmail.com>
2020-05-25 08:50:37 +0200

    Clément

--- scrap.py
+++ scrap.py
@@ -1,18 +1,14 @@
 import requests as rq
 from bs4 import BeautifulSoup as bs
 from pymongo import MongoClient
+import time
 
 client = MongoClient('mongodb://localhost:27017')
 db=client.indeed
 
-
 metiers = ['développeur', 'data scientist', 'data analyst', 'business intelligence']
 localisations = ['Paris', 'Lyon', 'Toulouse', 'Nantes', 'Bordeau', 'Nancy']
 contrats = ['permanent', 'contract', 'temporary', 'custom_1', 'apprenticeship', 'internship', 'subcontract', 'fulltime', 'parttime']
-
-# metiers = ['developpeur']
-# localisations = ['Paris']
-# contrats = ['permanent']
 
 # def ask(metier='', localisation='', contrat=''):
 #     if len(metier) > 0:
@@ -33,10 +29,25 @@ contrats = ['permanent', 'contract', 'temporary', 'custom_1', 'apprenticeship', 
 #     return metier, localisation, contrat
 
 def verification(metier, localisation, contrat):
+    test = True
     if len(contrat) > 0:
-        page = rq.get(f"https://www.indeed.fr/emplois?q={metier}&l={localisation}&jt={contrat}")
+        while test:
+            try:
+                page = rq.get(f"https://www.indeed.fr/emplois?q={metier}&l={localisation}&jt={contrat}")
+                test = False
+            except:
+                print('except')
+                time.sleep(2)
+                test = True
     else:
-        page = rq.get(f"https://www.indeed.fr/emplois?q={metier}&l={localisation}")
+        while test:
+            try:
+                page = rq.get(f"https://www.indeed.fr/emplois?q={metier}&l={localisation}")
+                test = False
+            except:
+                print('except')
+                time.sleep(2)
+                test = True
     soup = bs(page.content, 'html.parser')
     td = soup.find('td', id='resultsCol')
     if len(td.find_all('div', {'class':'invalid_location'})) > 0 or len(td.find_all('div', {'class':'no_results'})) > 0:
@@ -61,10 +72,8 @@ def id_exist(id_test):
 def id_link_title(soup, job):
     td = soup.find('td', id='resultsCol')
     for a in td.find_all('a', rel='noopener nofollow'):
-        if id_exist(a['id']):
-            print('Id déjà existente.')
-        else:
-            job['id'].append(a['id'])
+        if id_exist(a.find_parent('div')['data-jk']) == False or a.find_parent('div')['data-jk'] in job['id']:
+            job['id'].append(a.find_parent('div')['data-jk'])
             job['lien'].append('http://www.indeed.fr/{}'.format(a['href']))
             job['poste'].append(a.get_text()[1:])
     
@@ -83,14 +92,25 @@ def get_adress(soup, job):
 
 def with_links(links, job):
     for link in links:
-        page = rq.get(link)
+        test = True
+        while test:
+            try:
+                page = rq.get(link)
+                test = False
+            except:
+                print('except')
+                time.sleep(2)
+                test = True
         soup_temp = bs(page.content, 'html.parser')
         for div in soup_temp.find_all('div', {'class':'jobsearch-JobMetadataFooter'}):
             for child in div.find_all('div'):
                 child.decompose()
             job['publication'].append(div.get_text().split(' - ')[1])
-        for div in soup_temp.find_all('div', {'id':'jobDescriptionText'}):
-            job['description'].append(div.get_text())
+        if soup_temp.find_all('div', {'id':'jobDescriptionText'}) == []:
+            job['description'].append('')
+        else:
+            for div in soup_temp.find_all('div', {'id':'jobDescriptionText'}):
+                job['description'].append(div.get_text())
         if soup_temp.find_all('div', {'class':'icl-IconFunctional icl-IconFunctional--jobs icl-IconFunctional--md'}) == []:
             job['contrat'].append('')
         else:
@@ -102,43 +122,48 @@ def with_links(links, job):
              for icone in soup_temp.find_all('div', {'class':'icl-IconFunctional icl-IconFunctional--salary icl-IconFunctional--md'}):
                 job['salaire'].append(icone.find_parent('div').get_text())
 
-def get_next_page(soup):
-    a = soup.find_all('a', {'aria-label':'Suivant'})
-    for x in a:
-        next_link = 'http://www.indeed.fr/{}'.format(x['href'])
-        next_page = rq.get(next_link)
-        next_soup = bs(next_page.content, 'html.parser')
+def get_next_page(soup, i, metier, localisation, contrat):
+    test = True
+    while test :
+        try:
+            page = rq.get(f"https://www.indeed.fr/emplois?q={metier}&l={localisation}&jt={contrat}&start={i}")
+            test = False
+        except :
+            print('except')
+            time.sleep(2)
+            test = True
+    next_soup = bs(page.content, 'html.parser')
     return next_soup
 
-def suivant(soup):
-    if soup.find_all('a', {'aria-label':'Suivant'}) == []:
-        print('Pas de page suivante.')
-    elif soup.find_all('b', {'aria-current':'true', 'aria-label':'15'}) != []:
-        print('Dernière page.')
-    else:
-        print('Page suivante.')
-        job = reset_job()
-        next_soup = get_next_page(soup)
-        id_link_title(next_soup, job)
-        get_company(next_soup, job)
-        get_adress(next_soup, job)
-        with_links(job['lien'], job)
-        suivant(next_soup)
+def suivant(soup, metier, localisation, contrat):
+    for i in range(10, 1010, 10):
+        td = soup.find('td', id='resultsCol')
+        if td.find_all('a', rel='noopener nofollow') == None:
+            break
+        else:
+            job = reset_job()
+            next_soup = get_next_page(soup, i, metier, localisation, contrat)
+            id_link_title(next_soup, job)
+            get_company(next_soup, job)
+            get_adress(next_soup, job)
+            with_links(job['lien'], job)
+            add_db(job)
               
 def add_db(job):
     for i in range(len(list(job.values())[0])):
-        to_add = {
-            '_id':job.get('id')[i],
-            'lien':job.get('lien')[i],
-            'poste':job.get('poste')[i],
-            'entreprise':job.get('entreprise')[i],
-            'contrat':job.get('contrat')[i],
-            'lieu':job.get('lieu')[i],
-            'salaire':job.get('salaire')[i],
-            'publication':job.get('publication')[i],
-            'description':job.get('description')[i]
-        }
-        db['data'].insert_one(to_add)
+        if not id_exist(job['id'][i]):
+            to_add = {
+                '_id':job.get('id')[i],
+                'lien':job.get('lien')[i],
+                'poste':job.get('poste')[i],
+                'entreprise':job.get('entreprise')[i],
+                'contrat':job.get('contrat')[i],
+                'lieu':job.get('lieu')[i],
+                'salaire':job.get('salaire')[i],
+                'publication':job.get('publication')[i],
+                'description':job.get('description')[i]
+            }
+            db['data'].insert_one(to_add)
 
 def reset_job():
     job = {
@@ -152,14 +177,15 @@ def run():
     for metier in metiers:
         for localisation in localisations:
             for contrat in contrats:
+                print('Début pour %s à %s en %s'%(metier, localisation, contrat))
                 job = reset_job()
                 soup = verification(metier, localisation, contrat)
                 id_link_title(soup, job)
                 get_company(soup, job)
                 get_adress(soup, job)
                 with_links(job['lien'], job)
-                suivant(soup)
                 add_db(job)
+                suivant(soup, metier, localisation, contrat)
             print('Fin contrat pour %s à %s'%(metier, localisation))
         print('Fin localisation pour %s'%(metier))
     print('Fin. Youpi')
